82eb9c38fba990dc856be620b9eed88ac407b71f
mcchotaliya2005-jpg <mcchotaliya2005@gmail.com>
2026-01-16 11:58:53 +0530

    commit

---
+++ Core Java/practice/SwitchEx.java
@@ -0,0 +1,17 @@
+public class SwitchEx 
+{
+	public static void main(String[] args) 
+	{
+		int num = 2;
+		
+		switch(num)
+		{
+			case 1: System.out.println("English");
+			break;
+			case 2: System.out.println("Hindi");
+			break;
+			case 3: System.out.println("Gujarati");
+			break;
+		}
+	}
+}
