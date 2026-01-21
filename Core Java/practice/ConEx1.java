bceebf5898427a5a45814c1fe61b8fe271077512
mcchotaliya2005-jpg <mcchotaliya2005@gmail.com>
2026-01-21 14:05:27 +0530

    commit

---
+++ Core Java/practice/ConEx1.java
@@ -0,0 +1,16 @@
+package com;
+
+public class ConEx1 
+{
+	public ConEx1() 
+	{
+		System.out.println("Hello");
+	}
+	
+	public static void main(String[] args) 
+	{
+		ConEx1 c1 = new ConEx1();
+		System.out.println("Welcome");
+	}
+	
+}
---
+++ Core Java/practice/FinalEx.java
@@ -0,0 +1,16 @@
+package com.a3012;
+
+class G
+{
+	
+}
+
+public class FinalEx extends G
+{
+	public static void main(String[] args) 
+	{
+		int speed =90;
+		speed=120;
+		System.out.println(speed);
+	}
+}
