cdc8c4294f1e0909281ebc5a8c26248a2d3784fd
mcchotaliya2005-jpg <mcchotaliya2005@gmail.com>
2026-01-28 16:42:50 +0530

    commit

---
+++ Core Java/practice/8-1-26/FileEx1.java
@@ -0,0 +1,26 @@
+package com.a8;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileEx1 
+{
+	public static void main(String[] args) throws IOException
+	{
+		
+			String s = "Welcome to tops";
+			FileOutputStream fout = new FileOutputStream("H://hello.txt");
+			fout.write(s.getBytes());
+			System.out.println("Executed");
+	
+//		finally
+//		{
+//			int data = 10;
+//			System.out.println(data);
+//		}
+	}
+}
+
+//FileOutputStream - write
+//FileInputStream - read
+
---
+++ Core Java/practice/8-1-26/MyException.java
@@ -0,0 +1,9 @@
+package com.a8;
+
+class MyException extends Exception
+{
+	void error()
+	{
+		System.out.println("Error Printed");
+	}
+}
